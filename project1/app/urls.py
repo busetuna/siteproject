23cb5565364f0f5418c928419f027f59479f10e0
Buse Tuna <btuna4024@gmail.com>
2024-07-11 19:33:34 +0300

    contiuned

--- project1/app/urls.py
+++ project1/app/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("" , views.index)
+    path("" , views.index),
+    path("references/", views.references , name ='references'),
+    path("contact/", views.contact , name ='contact'),
+    path("language/", views.language , name = 'language'),
 ]
--- project1/app/views.py
+++ project1/app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request , "index.html")
+
+def references(request):
+    pass
+
+def contact(request):
+    pass
+
+def language(request):
+    pass
