991a2a29897e097ad51c0265ede74a76bbebf60c
Buse Tuna <btuna4024@gmail.com>
2024-07-16 18:08:23 +0300

    contiuned

--- project1/app/models.py
+++ project1/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class urunler(models.Model):
+    title = models.CharField(max_length= 50)
+    description = models.CharField(max_length= 400)
+    image =models.ImageField()
+
+
+class referanslarimiz(models.Model):
+    title = models.CharField(max_length= 50)
+    description = models.CharField(max_length= 400)
+    image =models.ImageField()
